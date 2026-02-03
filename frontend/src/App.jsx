e5f8a85fb8e5455b8bffab3aa3877b8b0d429f4f
ajeyajaz <ajeyajaz@gmail.com>
2026-02-03 08:24:23 +0530

    implement layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="layout">
+
+      <div className="nav bg-amber-200">nav</div>
+      <div className="main bg-blue-400">main</div>
+      <div className="sidebar bg-cyan-300">sidebar</div>
+
+    </div>
   );
 }
 
