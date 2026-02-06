ea7b498611b50531139a2449c627fced6ababc7a
ajeyajaz <ajeyajaz@gmail.com>
2026-02-06 07:45:34 +0530

    refactore : auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,11 +3,11 @@ import jwt from 'jsonwebtoken'
 
 export default function(req, res, next){
 
-    const authToken = req.get('x-auth-token');
+    const authToken = req.headers.authorization?.split(' ')[1];
     if(!authToken) return res.status(401).send('auth token is required.');
 
     jwt.verify(authToken, process.env.JWT_SECRET_KEY, function(err, decoded) {
-        if(err) return res.status(401).send('Invalid auth-token is required.');
+        if(err) return res.status(401).send('Invalid auth-token.');
 
         req.user = decoded;
         next();
