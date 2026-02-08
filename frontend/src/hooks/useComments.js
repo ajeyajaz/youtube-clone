376e5a66fcdb0245bd094d8e17f232b893429669
ajeyajaz <ajeyajaz@gmail.com>
2026-02-08 17:05:46 +0530

    implement useComments hook

---
+++ frontend/src/hooks/useComments.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import commentService from "../services/comment-service"
+
+
+
+export default (videoId) => {
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=> {
+
+        if(!videoId) return;
+
+        setError('');
+        setLoading(true);
+
+        const {request, cancel} = commentService.get(videoId)
+            request
+            .then(({ data })=> {
+                console.log('comments data: ', data);
+                setData(data);
+            })
+            .catch(ex => setError(ex.response?.data || 'something went wrong'))
+            .finally(() => setLoading(false))
+
+            return () => cancel();
+
+   }, [videoId])
+
+   return {data, error, loading};
+};
+
