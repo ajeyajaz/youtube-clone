f48c41c64b366af3a02cf8ae36b491910c7661a1
ajeyajaz <ajeyajaz@gmail.com>
2026-02-09 11:09:01 +0530

    create postIndicator component

---
+++ frontend/src/components/PostIndicator.jsx
@@ -0,0 +1,11 @@
+
+function PostIndicator() {
+  return (
+    <div className="w-full h-full">
+        <div className="w-5 h-5 border border-t-0 rounded-full animate-spin">
+        </div>
+    </div>
+  )
+}
+
+export default PostIndicator
