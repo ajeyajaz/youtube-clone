61894b83335db8546c4246555fbd943f757b3f3e
ajeyajaz <ajeyajaz@gmail.com>
2026-02-17 10:37:43 +0530

    prepare frontend to deploy

--- frontend/src/services/api-client.js
+++ frontend/src/services/api-client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://video-stream-platform.onrender.com',
     withCredentials: true,
 });
 
--- frontend/src/services/http-services.js
+++ frontend/src/services/http-services.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://video-stream-platform.onrender.com',
     withCredentials: true,
 });
 
