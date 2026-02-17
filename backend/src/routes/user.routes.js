184da84ecf0842ccb442726db21c7d8d7df2e407
ajeyajaz <ajeyajaz@gmail.com>
2026-02-17 10:26:05 +0530

    fix: asyncHandler typo

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,17 +1,17 @@
 import { register, login, updateAvatar } from "../controllers/user.controller.js";
 import authMiddleware from '../middlewares/auth.middleware.js'
 import express from 'express';
-import asynchandler from "../utils/asynchandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import {upload} from '../middlewares/multer.middleware.js'
 
 
 const router = express.Router();
 
-router.post('/register', asynchandler(register));
-router.post('/login', asynchandler(login));
+router.post('/register', asyncHandler(register));
+router.post('/login', asyncHandler(login));
 router.patch('/me/avatar',
     [authMiddleware, upload.single('avatar')],
-     asynchandler(updateAvatar)
+     asyncHandler(updateAvatar)
 );
 
 
